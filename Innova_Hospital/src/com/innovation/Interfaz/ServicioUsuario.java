7aa76c575709dae37c8116d3ef11dc7f2ee4c8b5
DouglasLemus <douglaslemusm@gmail.com>
2019-05-17 11:45:31 -0600

    Se agregan todas las clases necesarias para crear usuario -DELM

---
+++ Innova_Hospital/src/com/innovation/Interfaz/ServicioUsuario.java
@@ -0,0 +1,22 @@
+package com.innovation.Interfaz;
+
+import java.util.List;
+
+import com.innovation.modelo.Usuario;
+
+
+public interface ServicioUsuario {
+
+	public List<Usuario> mostra();
+	
+	public void Insertar(Usuario usuario);
+	
+	public Usuario Buscar(int id);
+	
+	public void Actualizar(Usuario usuario);
+	
+	public void Eliminar(int id);
+	
+	public String GetMensaje();
+	
+}
